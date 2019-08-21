b6516cbc0a5b7853fb039437526c6752264c2f4c
Ariel Silva <ceosilvajr@gmail.com>
2019-08-21 16:34:49 +0800

    - Added splash screen #1

--- app/src/main/java/com/ceosilvajr/starwars/MainActivity.kt
+++ app/src/main/java/com/ceosilvajr/starwars/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.ceosilvajr.starwars
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * @author ceosilvajr@gmail.com
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
     }
+
 }
